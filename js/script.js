fc9894a14a3d14438cb29339b6b000b7dc943fcc
ksv741 <ksv741@mail.ru>
2020-02-20 14:58:11 +0300

    day3 with mode function declofnum

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+const declOfNum = (n, titles, from) => {
+  
+  // return titles[n % 10 === 1 && n % 100 !== 11 ?
+  //       0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+  // Старый вариант
+
+  return titles[from === 'from' ? n % 10 === 1 && n % 100 !== 11 ? 1 : 2 : n % 10 === 1 && n % 100 !== 11 ?
+    0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+      
+}
+const Days = ['день', 'дня', 'дней'];
+
 const startBtn =  document.querySelector('.start-button'),
       mainForm = document.querySelector('.main-form'),
       firstScreen = document.querySelector('.first-screen'),
@@ -5,7 +17,12 @@ const startBtn =  document.querySelector('.start-button'),
       endBtn = document.querySelector('.end-button'),
       total = document.querySelector('.total'),
       fastRange = document.querySelector('.fast-range'),
-      totalPriceSum = document.querySelector('.total_price__sum');
+      totalPriceSum = document.querySelector('.total_price__sum'),
+      typeSite = document.querySelector('.type-site'),
+      maxDeadline = document.querySelector('.max-deadline'),
+      rangeDeadline = document.querySelector('.range-deadline'),
+      deadlineValue = document.querySelector('.deadline-value');
+      
 const DATA = {
   whichSite: ['landing', 'multiPage', 'onlineStore'],
   price: [4000, 8000, 26000],
@@ -16,15 +33,27 @@ const DATA = {
   metrikaYandex: [500, 1000, 2000],
   analyticsGoogle: [850, 1350, 3000],
   sendOrder: 500,
-  deadlineDay: [[2, 7], [3, 10], [7, 14]],
+  deadlineDay: [[2, 7], [3, 10], [2, 14]],
   deadlinePercent: [20, 17, 15]
 }
+
 const showElem = (el) => {
     el.style.display = 'block';
 }
+
 const hideElem = (el) => {
     el.style.display = 'none';
 }
+
+const renderTextContent = (total, site, maxDeadlineDay, minDeadlineDay) => {
+  maxDeadline.textContent = `${maxDeadlineDay} ${declOfNum(maxDeadlineDay, Days, 'from')}`; 
+  totalPriceSum.textContent = total;
+  typeSite.textContent = site;
+  rangeDeadline.max = maxDeadlineDay;
+  rangeDeadline.min = minDeadlineDay;
+  deadlineValue.textContent = `${rangeDeadline.value} ${declOfNum(rangeDeadline.value, Days)}`;   
+}
+
 const handlerCallBackForm = (event) => {
   const target = event.target;
   if(target.classList.contains('want-faster')){
@@ -52,10 +81,14 @@ const handlerCallBackForm = (event) => {
     }
   }
 }
+
 const priceCalculation = (elem) => {
   let result = 0, 
       index = 0,
-      options = [];
+      options = [],
+      site =  '',
+      maxDeadlineDay = DATA.deadlineDay[0][1],
+      minDeadlineDay = DATA.deadlineDay[0][0];
   if(elem.name === 'whichSite'){
     for(const item of formCalculate.elements){
       if(item.type === 'checkbox'){
@@ -68,8 +101,13 @@ const priceCalculation = (elem) => {
   for(const item of formCalculate.elements){
     if (item.name === "whichSite" && item.checked){
       index = DATA.whichSite.indexOf(item.value);
+      site = item.dataset.site;
+      maxDeadlineDay = DATA.deadlineDay[index][1];
+      minDeadlineDay = DATA.deadlineDay[index][0];
     }else if (item.classList.contains('calc-handler') && item.checked){
       options.push(item.value);
+    }else if (item.name === 'deadline'){
+      nowDeadlineDay = item.value;
     }
   }
   options.forEach(key => {
@@ -77,7 +115,7 @@ const priceCalculation = (elem) => {
       if( key === 'sendOrder'){
         result += DATA[key]
       }else{
-        result += DATA.price[index] * DATA[key]/100
+        result += DATA.price[index] * DATA[key]/100;
       }
     }else{
       if(key === 'desktopTemplates'){
@@ -89,7 +127,7 @@ const priceCalculation = (elem) => {
   })
   result += DATA.price[index];
 
-  totalPriceSum.textContent = result;
+  renderTextContent(result, site, maxDeadlineDay, minDeadlineDay);
 }
 startBtn.addEventListener('click', () => {
     showElem(mainForm);
